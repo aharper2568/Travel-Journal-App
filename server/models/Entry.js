a5cd06f66b69e646f512b267214035fe617c1968
Bryson <brysob7617@gmail.com>
2024-10-02 20:49:56 -0400

    added model for entry and ref entry in user model

---
+++ server/models/Entry.js
@@ -0,0 +1,34 @@
+const { Schema, model, Types } = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const entrySchema = new Schema(
+    {
+        author: {
+            type: Types.ObjectId,
+            ref: 'User',
+        },
+        title: {
+            type: String,
+            required: true,
+        },
+        location: {
+            type: String,
+            required: true,
+        },
+        date: {
+            type: String,
+            required: true,
+        },
+        picture: {
+            type: String,
+        },
+        content: {
+            type: String,
+            required: true,
+        },
+    })
+    const Entry = model('Entry', entrySchema);
+
+    module.exports = Entry;
+    
+    ex
