96dac37f211c99a26a2e787bcdaa0aedb710b122
Bryson <brysob7617@gmail.com>
2024-10-07 19:31:25 -0400

    added queries for search user, entries, and entries by ID

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -20,12 +20,56 @@ export const QUERY_SINGLE_USER = gql`
   }
 `;
 
+export const QUERY_SEARCH_USERS = gql`
+  query searchUsers($term: String) {
+    users(term: $term) {
+    _id
+    username
+    entries 
+   }
+  }
+`;
+
 export const QUERY_ME = gql`
   query me {
     me {
       _id
       username
       entries
+    }
+  }
+`;
+
+export const QUERY_ENTRIES = gql`
+  query entries {
+    entries {
+      _id
+      content
+      date
+     location
+     picture
+     title
+      author {
+       _id
+       username
+      }
+    }
+  }
+`;
+
+export const QUERY_SINGLE_ENTRY = gql`
+  query entry($entryId: ID!) {
+    entry(entryId: $entryId) {
+    _id
+    content
+    date
+    location
+    picture
+    title
+      author {
+       _id
+       username
+      }
     }
   }
 `;
