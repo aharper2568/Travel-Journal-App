4458eded5e560d596a2140d681b82678c0b31d2a
aharper2568 <anthonyharper568@gmail.com>
2024-10-07 21:44:05 -0400

    added token to profile mutation

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -3,6 +3,7 @@ import { gql } from '@apollo/client';
 export const ADD_PROFILE = gql`
   mutation addUser($username: String!, $email: String!, $password: String!) {
     addUser(username: $username, email: $email, password: $password) {
+      token
       user {
         _id
         username
