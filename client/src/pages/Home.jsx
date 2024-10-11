74d4a90c5915a2e4342fa506f7f097bc26ffcf99
aharper2568 <anthonyharper568@gmail.com>
2024-10-10 21:32:39 -0400

    added entry card preview

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -5,9 +5,29 @@ import { Link } from 'react-router-dom';
 
 const UserLinks = ({ users }) => {
   return users?.map(user => (
-    <p key={user._id}>
-      <Link to={`/profiles/${user._id}`}>{user.username}</Link>
-    </p>
+    <div className="border my-2" key={user._id}>
+      <p>
+        <Link to={`/profiles/${user._id}`}>{user.username}</Link>
+      </p>
+      {user.entries.length > 0 ? (
+        <div className="entry-preview">
+          <h4>{user.entries[0].title}</h4>
+          <p><strong>Date:</strong> {user.entries[0].date}</p>
+          <p><strong>Location:</strong> {user.entries[0].location}</p>
+          <p><strong>Content:</strong> {user.entries[0].content}</p>
+          {user.entries[0].picture && (
+            <img
+              src={user.entries[0].picture}
+              alt={user.entries[0].title}
+              style={{ maxWidth: '100%', maxHeight: '300px', objectFit: 'cover' }}
+            />
+          )}
+         
+        </div>
+      ) : (
+        <p>No entries available.</p>
+      )}
+    </div>
   ));
 }
 
